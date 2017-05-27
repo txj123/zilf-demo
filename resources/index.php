c8ed979f009c6bf7cdf57567a8474e9dc5390808
txj123 <597196313@qq.com>
2017-05-27 10:56:13 +0800

    修改代码
    删除不需要的文件，修改首页显示的css

---
+++ resources/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="renderer" content="webkit">
+    
+    <title>Zilf</title>
+
+    <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .content {
+                text-align: center;
+                margin-top: 168px;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+</head>
+<body>
+
+	<div class="content">
+        <div class="title m-b-md">
+            Zilf框架 欢迎使用！
+        </div>
+
+        <div class="links">
+            <a href="https://github.com/txj123/zilf-docs">文档（Documentation）</a>
+            <a href="https://github.com/txj123/zilf">Zilf</a>
+            <a href="https://github.com/txj123/zilf-demo">Zilf-demo</a>
+            <a href="https://github.com/txj123/zilf-demo">GitHub</a>
+        </div>
+    </div>
+    
+</body>
+</html>
